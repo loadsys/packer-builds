68f09380c43d5496b7f70891559568f6b86cf323
Rick Guyer <ricoguyer@gmail.com>
2017-04-21 00:04:32 -0500

    Allow PHP 5.6 to install nginx or apache libs

--- scripts/ubuntu/php5.6.sh
+++ scripts/ubuntu/php5.6.sh
@@ -1,4 +1,11 @@
 # php 5.6
+
+# Webservers supported by this script
+# Nginx uses WEBSERVER=nginx
+# Apache uses WEBSERVER=apache
+
+[ "$WEBSERVER" = "nginx" ] && WEBPKG="php5.6-fpm" || WEBPKG="libapache2-mod-php5.6"
+
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update -y
-sudo apt-get install -y php5.6 php5.6-fpm php5.6-curl php5.6-intl php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-gd php5.6-xml php5.6-soap
+sudo apt-get install -y php5.6 $WEBPKG php5.6-curl php5.6-intl php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-gd php5.6-xml php5.6-soap
